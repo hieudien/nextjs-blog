d4ed2b21cad3fc2133b79d421854c99e4530dfcd
Hieuu Doan <hieu@wiserbot.com>
2022-04-18 08:41:48 +0700

    Update text and add new post

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Giang cute'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Blog của Giang'
 
 export default function Layout({ children, home }) {
   return (
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Blog của Giang"
         />
         <meta
           property="og:image"
@@ -64,7 +64,7 @@ export default function Layout({ children, home }) {
       {!home && (
         <div className={styles.backToHome}>
           <Link href="/">
-            <a>← Back to home</a>
+            <a>← Trang chủ</a>
           </Link>
         </div>
       )}
